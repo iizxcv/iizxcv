4c9d209db6caf1369edb10d369e77e466a07dac3
iizxcv <jmlee9471@gmail.com>
2025-09-30 13:54:14 +0000

    [Silver II] Title: DFS와 BFS, Time: 8 ms, Memory: 6132 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -5,9 +5,9 @@
 #include <deque>
 using namespace std;
 
-void DFS(vector<vector<int>> g, int start)
+void DFS(vector<vector<int>> *g, int start)
 {
-    vector<int> visited(g.size(), 0);
+    vector<int> visited(g->size(), 0);
     vector<int> stack;
     stack.push_back(start);
     while (!stack.empty())
@@ -20,9 +20,9 @@ void DFS(vector<vector<int>> g, int start)
         cout << cur << " ";
 
         visited[cur] = 1;
-        for (int i = g.size() - 1; i > 0; i--)
+        for (int i = g->size() - 1; i > 0; i--)
         {
-            if (g[cur][i] == 1) //&& find(stack.begin(), stack.end(), i) == stack.end()
+            if ((*g)[cur][i] == 1) //&& find(stack.begin(), stack.end(), i) == stack.end()
             {
                 stack.push_back(i);
             }
@@ -30,9 +30,9 @@ void DFS(vector<vector<int>> g, int start)
     }
 }
 
-void BFS(vector<vector<int>> g, int start)
+void BFS(vector<vector<int>> *g, int start)
 {
-    vector<int> visited(g.size(), 0);
+    vector<int> visited(g->size(), 0);
     deque<int> queue;
     queue.push_back(start);
 
@@ -47,9 +47,9 @@ void BFS(vector<vector<int>> g, int start)
         cout << cur << " ";
         visited[cur] = 1;
 
-        for (int i = 1; i < g.size() + 1; i++)
+        for (int i = 1; i < g->size() + 1; i++)
         {
-            if (g[cur][i] == 1)
+            if ((*g)[cur][i] == 1)
             {
                 queue.push_back(i);
             }
@@ -84,7 +84,7 @@ int main()
     //     cout << "\n";
     // }
 
-    DFS(g, v);
+    DFS(&g, v);
     cout << "\n";
-    BFS(g, v);
+    BFS(&g, v);
 }
