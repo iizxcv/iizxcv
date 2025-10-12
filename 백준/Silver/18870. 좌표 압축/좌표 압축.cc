022f334df8e6f8c84a48f2d04925d2aa3b2d6cc9
iizxcv <jmlee9471@gmail.com>
2025-10-13 02:13:11 +0900

    [Silver II] Title: 좌표 압축, Time: 876 ms, Memory: 13944 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -3,6 +3,41 @@
 #include <unordered_map>
 #include <algorithm>
 using namespace std;
+
+void solution(std::vector<int> &arr)
+{
+    std::vector<int> copy_arr = arr;
+    sort(copy_arr.begin(), copy_arr.end());
+    copy_arr.erase(unique(copy_arr.begin(), copy_arr.end()), copy_arr.end());
+
+    // for (int i = 0; i < arr.size(); i++)
+    // {
+    //     auto it = find(copy_arr.begin(), copy_arr.end(), arr[i]);
+    //     if (it != copy_arr.end())
+    //     {
+    //         arr[i] = int(it - copy_arr.begin());
+    //     }
+    // }
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int default_val = -1;
+        for (int li = 0, ri = copy_arr.size() - 1; li <= ri;)
+        {
+            int mid = (li + ri) / 2;
+            if (copy_arr[mid] < arr[i])
+            {
+                default_val = max(default_val, mid);
+                li = mid + 1;
+            }
+            else
+            {
+                ri = mid - 1;
+            }
+        }
+        arr[i] = default_val + 1;
+    }
+}
+
 int main()
 {
     int n;
@@ -16,21 +51,10 @@ int main()
         arr.push_back(in);
     }
     vector<int> cp_arr = arr;
-    sort(cp_arr.begin(), cp_arr.end());
-    unordered_map<int, int> umap;
-    umap.reserve(n);
 
-    int cnt = 0;
-    for (int i = 0; i < cp_arr.size(); i++)
+    solution(arr);
+    for (auto a : arr)
     {
-        if (umap.find(cp_arr[i]) == umap.end())
-        {
-            umap[cp_arr[i]] = cnt;
-            cnt++;
-        }
-    }
-    for (int i = 0; i < arr.size(); i++)
-    {
-        cout << umap[arr[i]] << " ";
+        cout << a << " ";
     }
 }
