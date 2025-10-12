6c599d67a7f512d12bd60e06be528df547285aa2
iizxcv <jmlee9471@gmail.com>
2025-10-13 00:56:52 +0900

    [Silver II] Title: 좌표 압축, Time: 1072 ms, Memory: 35336 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr;
+    for (int i = 0; i < n; i++)
+    {
+        int in;
+        cin >> in;
+        arr.push_back(in);
+    }
+    vector<int> cp_arr = arr;
+    sort(cp_arr.begin(), cp_arr.end());
+    unordered_map<int, int> umap;
+
+    int cnt = 0;
+    for (int i = 0; i < cp_arr.size(); i++)
+    {
+        if (umap.find(cp_arr[i]) == umap.end())
+        {
+            umap[cp_arr[i]] = cnt;
+            cnt++;
+        }
+    }
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << umap[arr[i]] << " ";
+    }
+}
